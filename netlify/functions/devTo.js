721c680a4aa2609097439d51bdd17c6dfee760c0
sneha025 <simaurya96@gmail.com>
2022-02-05 00:59:48 +0530

    added new content

--- netlify/functions/devTo.js
+++ netlify/functions/devTo.js
@@ -3,32 +3,32 @@ const axios = require("axios");
 const handler = async (event) => {
   // get data from database
   //create a dummy data for dev to blog posting.
-  var devToPost = JSON.stringify({
-    article: {
-      title: "Netlify Schedule",
-      body_markdown: "Test blog for testing netlify Scheduling",
-      published: false,
-    },
-  });
-  var config = {
-    method: "post",
-    url: "https://dev.to/api/articles",
-    headers: {
-      "Content-Type": "application/json",
-      "api-key": "r7p6nt1py397dGDfkdCNznfr",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    },
-    data: devToPost,
-  };
+  //   var devToPost = JSON.stringify({
+  //     article: {
+  //       title: "Netlify Schedule",
+  //       body_markdown: "Test blog for testing netlify Scheduling",
+  //       published: false,
+  //     },
+  //   });
+  //   var config = {
+  //     method: "post",
+  //     url: "https://dev.to/api/articles",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "api-key": "r7p6nt1py397dGDfkdCNznfr",
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  //     },
+  //     data: devToPost,
+  //   };
 
-  await axios(config)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log("ERROR", err);
-    });
+  //   await axios(config)
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log("ERROR", err);
+  //     });
   return {
     statusCode: 200,
     body: "rebuild",
