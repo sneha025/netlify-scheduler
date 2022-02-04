69c875cca4715bf62981afb3f65760f0ce5d7ba0
sneha025 <simaurya96@gmail.com>
2022-02-04 14:22:37 +0530

    added scheduler

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,10 +1,11 @@
-const { schedule } = require("@netlify/functions");
-const handler = async () => {
+// const { schedule } = require("@netlify/functions");
+const handler = async (event, context) => {
+  console.log("Received event:", event);
   return {
     statusCode: 200,
-    body: "hello",
+    body: "welcome to function",
   };
 };
 
-// module.exports.handler = schedule("@hourly", handler);
+// module.exports.handler = schedule("* * * * *", handler);
 module.exports.handler = handler;
